96c8fbcd722668870f405b5bafe41750cb9787f2
Snake <jrodriguezp7@gmail.com>
2020-03-11 11:41:27 -0400

    Added Solidity Contract

---
+++ contracts/election.sol
@@ -0,0 +1,14 @@
+pragma solidity  ^0.5.16;
+
+contract election{
+	//Store candidate
+	//Read Candidate
+	//Constructor
+	string public candidate;
+
+	constructor() public{
+		candidate = "Candidate";
+	}
+
+
+}
